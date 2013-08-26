b4a63d8d5353be788667b9053e2db77e76001bd0
Amos Lanka <amoslanka@gmail.com>
2013-08-26 13:32:00 -0700

    erp

--- lib/standards/basic_http_auth.rb
+++ lib/standards/basic_http_auth.rb
@@ -1,7 +1,7 @@
 module Standards
   module BasicHttpAuth
     # Is Enabled if the environment variables are present.
-    if ENV['HTTP_USERNAME'].present? and ENV['HTTP_PASSWORD'].present?)
+    if ENV['HTTP_USERNAME'].present? and ENV['HTTP_PASSWORD'].present?
       Rails.application.config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", Rails.env.to_s.titleize) do |u, p|
         [u, p] == [ENV['HTTP_USERNAME'], ENV['HTTP_PASSWORD']]
       end
